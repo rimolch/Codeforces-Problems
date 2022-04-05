b541f591438168e0672ca86cf7e2b102b5d60dc8
rimolch <67545874+rimolch@users.noreply.github.com>
2022-04-05 23:19:41 +0600

    Create bun.cpp

---
+++ B. Unique Bid Auction/bun.cpp
@@ -0,0 +1,45 @@
+# vector and map 
+#include<bits/stdc++.h>
+#include<math.h>
+ 
+using namespace std;
+ 
+void solve()
+{
+        int  n;
+        cin>>n;
+        vector<int>ar(n);
+        map<int, int>mp;
+        for(int i =0;i<n;i++)
+        {
+            int x;
+            cin>>x;
+            ar[i]=x;
+            mp[x]++;
+        }
+        for(auto it: mp)
+        {
+            if(it.second ==1)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(ar[j]==it.first)
+                    {
+                        cout<<j+1<<endl;
+                        return ;
+                    }
+                }
+            }
+        }
+    cout<<-1<<endl;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
