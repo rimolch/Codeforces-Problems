047b12d9c5fb0046b8d06fbfdc26c053ce325b60
rimolch <67545874+rimolch@users.noreply.github.com>
2022-07-05 10:58:40 +0600

    Create crs.cpp

---
+++ STL_PROBLEMS/Map/C. Registration system/crs.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    map<string,int>mp;
+    for(int i=0;i<n;i++){
+        string s;
+        cin>>s;
+        if(mp[s]==0)cout<<"OK"<<endl;
+        else cout<<s<<mp[s]<<endl;
+        mp[s]++;
+    }
+    return 0;
+}
