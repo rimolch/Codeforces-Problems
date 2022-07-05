75b61fba673988c3724b37624b8e3df22dcf91e6
rimolch <67545874+rimolch@users.noreply.github.com>
2022-07-05 10:42:21 +0600

    Create arl.cpp

---
+++ STL_PROBLEMS/Vector/A. Rank List/arl.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,k;
+    cin>>n>>k;
+    vector<pair<int,int>> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i].first>>v[i].second;
+        v[i].first*=-1;
+    }
+    sort(v.begin(),v.end());
+    int ans=0;
+    for(auto u:v){
+       if(u==v[k-1]){// u is a pair of int ,(k-1)th jhe team ta ache setar soman penalty and solved koijoner ache 
+        ans++;
+       }
+    }
+   cout<<ans<<endl;
+    return 0;
+}
