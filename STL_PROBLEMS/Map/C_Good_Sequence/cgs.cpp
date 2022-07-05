1bbc0fa2edc4c26a785e164ad15c601dcf50daca
rimolch <67545874+rimolch@users.noreply.github.com>
2022-07-05 11:00:38 +0600

    Create cgs.cpp

---
+++ STL_PROBLEMS/Map/C_Good_Sequence/cgs.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    map<int,int>mp;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+        mp[v[i]]++;
+    }
+    int ans =0;
+    for(auto u:mp){
+       if(u.second>=u.first) {
+        ans+=(u.second-u.first);
+       }
+       else{
+        ans+=u.second;
+       }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
