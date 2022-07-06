13c3ae9803bbc3248728724d11e208797e93fbe5
rimolch <67545874+rimolch@users.noreply.github.com>
2022-07-06 07:57:42 +0600

    Create bp.cpp

---
+++ STL_PROBLEMS/Stack/Balanced_parentheisi/bp.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+bool isbalanced(char a,char b){
+    return (a=='('&&b==')')||(a=='{'&&b=='}')||(a=='['&&b==']');
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin>>n;
+    while(n--){
+        string s;
+        cin>>s;
+        stack<char>st;
+         int done =1;
+        for(auto u:s){
+            if(u=='('||u=='{'||u=='['){
+              st.push(u);
+            }
+            else{
+                if(st.empty()){
+                    done =0;
+                    break;
+                }
+                else{
+                    if(isbalanced(st.top(),u)){
+                       st.pop();
+                    }
+                    else{
+                        done=0;
+                        break;
+                    }
+                }
+            }
+        }
+        if(!st.empty()){
+            done=0;
+        }
+        if(done==1)cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
