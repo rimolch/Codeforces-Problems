61a9de6c68d4d54ccffa890036a895bf5e251cb1
rimolch <67545874+rimolch@users.noreply.github.com>
2022-07-12 12:01:21 +0600

    Update raads.cpp

--- STL_PROBLEMS/Stack/1047. Remove All Adjacent Duplicates In String/raads.cpp
+++ STL_PROBLEMS/Stack/1047. Remove All Adjacent Duplicates In String/raads.cpp
@@ -3,8 +3,8 @@ public:
     string removeDuplicates(string s) {
         string s1="";
         for(int i=0;i<s.size();i++){
-            if(s1.size()>0 && s1.back()==s[i]){
-                s1.pop_back();
+            if(s1.size()>0 && s1.back()/*if the previous stack value is equal with current value */==s[i]){
+                s1.pop_back();//then pop_back() the previous stack value
             }
             else{
                 s1.push_back(s[i]);
