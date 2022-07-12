64ea8c4daea4807beeea149b8325788470bba5cb
rimolch <67545874+rimolch@users.noreply.github.com>
2022-07-12 12:14:25 +0600

    Update raads.cpp

--- STL_PROBLEMS/Stack/1047. Remove All Adjacent Duplicates In String/raads.cpp
+++ STL_PROBLEMS/Stack/1047. Remove All Adjacent Duplicates In String/raads.cpp
@@ -13,3 +13,26 @@ public:
         return s1;
     }
 };
+// main 
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    string s;
+    cin>>s;
+    string c="";
+    for(int i=0;i<s.size();i++){
+        if(c.size()!=0&& c.back()==s[i]){
+            c.pop_back();
+        }
+        else{
+            c.push_back(s[i]);
+        }
+    }
+    cout<<c<<endl;
+    return 0;
+}
