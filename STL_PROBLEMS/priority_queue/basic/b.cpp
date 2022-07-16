251968d93139979c181628547f8e6a571ae9ac80
rimolch <67545874+rimolch@users.noreply.github.com>
2022-07-16 08:12:12 +0600

    Create b.cpp

---
+++ STL_PROBLEMS/priority_queue/basic/b.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    priority_queue<int>pq;
+    pq.push(2);
+     pq.push(1);
+      pq.push(2);
+       pq.push(3);
+        pq.push(5);
+         pq.push(7);
+     while(!pq.empty()){
+        cout<<pq.top()<<endl;
+        pq.pop();
+     }
+    return 0;
+}
